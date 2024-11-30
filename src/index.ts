afdf9fa610ec20ab341ec14a9eedfb1ca91f340a
StNekroman <stnekroman@gmail.com>
2024-11-30 18:28:45 +0200

    export Type only from Types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 export { Arrays } from "./Arrays";
-export { Deffered } from './Deffered';
+export { Deffered } from "./Deffered";
 export { Functions } from "./Functions";
 export { GroupBy } from "./GroupBy";
 export { Objects } from "./Objects";
 export { Sorter } from "./Sorter";
 export { Throttle } from "./throttle/Throttle";
-export { Types } from "./Types";
+export type { Types } from "./Types";
 
 export * from "./caching";
 export * from "./decorators";
-
