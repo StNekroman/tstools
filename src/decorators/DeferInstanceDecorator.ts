27006baa3087ab41b350a76f67a01ce5444c3c13
Roman Nefedov <roman@stitch.ai>
2025-04-18 17:38:07 +0300

    0.2.7

--- src/decorators/DeferInstanceDecorator.ts
+++ src/decorators/DeferInstanceDecorator.ts
@@ -5,23 +5,33 @@ interface DeferInstanceDecoratorsMetadata<C extends Object> {
 }
 
 const DeferInstanceDecoratorsMetadataSymbol = Symbol('DeferInstanceDecoratorsMetadata');
-function getDeferInstanceDecoratorsMetadata<C extends Object>(prototype: any): DeferInstanceDecoratorsMetadata<C> {
-  return prototype[DeferInstanceDecoratorsMetadataSymbol];
+function getDeferInstanceDecoratorsMetadata<C extends Object>(
+  target: any
+): DeferInstanceDecoratorsMetadata<C> | undefined {
+  if (target.hasOwnProperty(DeferInstanceDecoratorsMetadataSymbol)) {
+    return target[DeferInstanceDecoratorsMetadataSymbol];
+  }
+  return undefined;
 }
-function getOrCreateDeferInstanceDecoratorsMetadata<C extends Object>(
-  prototype: any
-): DeferInstanceDecoratorsMetadata<C> {
-  const metadata: DeferInstanceDecoratorsMetadata<C> =
-    prototype[DeferInstanceDecoratorsMetadataSymbol] ??
-    (prototype[DeferInstanceDecoratorsMetadataSymbol] = {
+function getOrCreateDeferInstanceDecoratorsMetadata<C extends Object>(target: any): DeferInstanceDecoratorsMetadata<C> {
+  let metadata: DeferInstanceDecoratorsMetadata<C>;
+  if (target.hasOwnProperty(DeferInstanceDecoratorsMetadataSymbol)) {
+    metadata = target[DeferInstanceDecoratorsMetadataSymbol];
+  } else {
+    metadata = {
       decorators: [],
-    } as DeferInstanceDecoratorsMetadata<C>);
+    };
+    Object.defineProperty(target, DeferInstanceDecoratorsMetadataSymbol, {
+      value: metadata,
+    });
+  }
   return metadata as DeferInstanceDecoratorsMetadata<C>;
 }
 
-export function applyInstanceDecorators<C extends Object>(
+export function applyInstanceDecorators<C extends Object, CTR extends Types.Newable<C> | { prototype: unknown }>(
   instance: C,
-  metadata: DeferInstanceDecoratorsMetadata<C> = getDeferInstanceDecoratorsMetadata<C>(instance.constructor.prototype)
+  ctr: CTR,
+  metadata: DeferInstanceDecoratorsMetadata<C> | undefined = getDeferInstanceDecoratorsMetadata<C>(ctr.prototype)
 ): void {
   if (metadata) {
     for (const [propertyName, decorator] of metadata.decorators) {
@@ -31,15 +41,17 @@ export function applyInstanceDecorators<C extends Object>(
 }
 
 export function RunInstanceDecorators<C extends Object>() {
-  return <CTR extends Types.Newable<C> | { prototype: C }>(ctr: CTR): CTR => {
-    const metadata: DeferInstanceDecoratorsMetadata<C> = getDeferInstanceDecoratorsMetadata<C>(ctr.prototype);
+  return <CTR extends Types.Newable<C> | { prototype: unknown }>(ctr: CTR): CTR => {
+    const metadata: DeferInstanceDecoratorsMetadata<C> | undefined = getDeferInstanceDecoratorsMetadata<C>(
+      ctr.prototype
+    );
     if (!metadata) {
       return ctr;
     }
     const newCtr = class extends (ctr as Types.Newable) {
       constructor(...args: unknown[]) {
         super(...args);
-        applyInstanceDecorators(this as unknown as C, metadata);
+        applyInstanceDecorators(this as unknown as C, ctr, metadata);
       }
     } as CTR;
     Object.defineProperty(newCtr, 'name', { value: (ctr as Types.Newable<C>).name });
