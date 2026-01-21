6e91160a7b732db4822a0dc6cb082db8b34cb728
StNekroman <stnekroman@gmail.com>
2026-01-21 14:51:11 +0200

    0.7.0

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export { GroupBy } from './GroupBy';
 export { Objects } from './Objects';
 export { NoSuchElementException } from './optional/NoSuchElementException';
 export { Optional } from './optional/Optional';
-export { Failure, Result, Success, type FailureData, type SuccessData } from './Result';
+export { Failure, Result, Success, type ResultData, type FailureData, type SuccessData } from './Result';
 export { Sorter } from './Sorter';
 export { Throttle } from './throttle/Throttle';
 export type { Types } from './Types';
